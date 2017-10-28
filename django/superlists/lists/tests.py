b138a13aea42bec5b2ed48e8df975fb35f1cf024
iyamoto <iyamoto@gmail.com>
2017-10-28 17:48:04 +0000

    uploading

--- django/superlists/lists/tests.py
+++ django/superlists/lists/tests.py
@@ -9,12 +9,14 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, 'home.html')
 
     def test_can_save_a_POST_request(self):
-        # TODO POST test is too long?
-        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.client.post('/', data={'item_text': 'A new list item'})
 
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
 
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], '/')
@@ -23,6 +25,15 @@ class HomePageTest(TestCase):
         self.client.get('/')
 
         self.assertEqual(Item.objects.count(), 0)
+
+    def test_displays_all_list_items(self):
+        self.client.post('/', data={'item_text': 'itemey 1'})
+        self.client.post('/', data={'item_text': 'itemey 2'})
+
+        response = self.client.get('/')
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
 
 
 class ItemModelTest(TestCase):
