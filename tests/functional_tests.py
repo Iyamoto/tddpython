a40a636dfdc0ea76b560ebcf823e59c108e4532d
iyamoto <iyamoto@gmail.com>
2017-10-29 18:49:43 +0300

    adding bootstrap

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -122,5 +122,18 @@ class NewVisitorTest(unittest.TestCase):
 
         # Satisfied, they both go back to sleep
 
+    def test_layout_and_styling(self):
+        # Edith goes to the home page
+        self.browser.get(self.djangourl )
+        self.browser.set_window_size(1024, 768)
+
+        # She notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
 if __name__ == '__main__':
     unittest.main()
