33d1f2d68fa30e863d0c56c0cd4b2fcd3c2cc2d2
iyamoto <iyamoto@gmail.com>
2017-10-29 12:19:54 +0300

    refactoring

--- django/start.sh
+++ django/start.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 project='superlists'
+appname='lists'
 
 cd /vagrant/django/
 # django-admin startproject ${project}
 cd ${project}
-# python manage.py startapp lists
+# python manage.py startapp ${appname}
 
 # Clear DB
 rm db.sqlite3
@@ -19,6 +20,7 @@ unittestresult=$?
 if [ ${unittestresult} -eq 0 ]; then
     # Django web server
     # python manage.py runserver 0.0.0.0:8000
+
     # Gunicorn
     gunicorn --env DJANGO_SETTINGS_MODULE=${project}.settings ${project}.wsgi -w 4 -b :8000
 fi
